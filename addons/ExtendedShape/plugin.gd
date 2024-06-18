c764b137f378930975c0efae1b5ee990584c9da1
Healleu <garnier.romain@outlook.com>
2024-06-18 19:56:03 +0200

    Add version update check

--- addons/ExtendedShape/plugin.gd
+++ addons/ExtendedShape/plugin.gd
@@ -1,15 +1,44 @@
 @tool
 extends EditorPlugin
 
-var version : String = ""
+var _http : HTTPRequest = null
+var _version : String = ""
+var _url : String = "https://raw.githubusercontent.com/Healleu/ExtendedShape/main/addons/ExtendedShape/plugin.cfg"
 
 func _enter_tree() -> void :
+	# Het plugin current version
+	_version = get_plugin_version()
+	
+	# Request lastest plugin version in Github
+	_http = HTTPRequest.new()
+	call_deferred("add_child", _http)
+	await _http.ready
+	_http.request_completed.connect(_on_http_request_request_completed)
+	_http.request(_url)
+	
+	# Initialize
 	add_custom_type("ConeShape2D", "ConvexPolygonShape2D", preload("2D/Cone/ConeShape2D.gd"), preload("2D/Cone/ConeShape2D.svg"))
-	version = get_plugin_version()
-	print("Plugin Extended Shape " + version + " initialized!")
+	print("Plugin ExtendedShape " + _version + " initialized!")
 	return
 	
 func _exit_tree() -> void :
+	# Deinitialize
 	remove_custom_type("ConeShape2D")
-	print("Plugin Extended Shape " + version + " deinitialized!")
+	print("Plugin ExtendedShape " + _version + " deinitialized!")
+	return
+
+## Called when the request to the repository is completed, parses the config file and sets the newest version.
+func _on_http_request_request_completed(result : int, response_code : int, headers : PackedStringArray, body : PackedByteArray):
+	var config = ConfigFile.new()
+	var err = config.parse(body.get_string_from_ascii())
+	
+	if err == OK :
+		var newest_version = config.get_value("plugin", "version")
+		config.clear()
+		if newest_version > _version :
+			print("New version of ExtendedShape is available!")
+			print("actual : " + _version + " / lastest : " + newest_version)
+	else :
+		print("Fail to get the lastest version of ExtendedShape")
+	_http.call_deferred("queue_free")
 	return
